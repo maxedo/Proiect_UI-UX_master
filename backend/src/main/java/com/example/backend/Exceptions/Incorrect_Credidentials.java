4d15a8d926748348082f0842e3a4073b508ff489
maxedo <maxitiansergiu@gmail.com>
2024-05-07 18:06:59 +0300

    backend dev change

--- backend/src/main/java/com/example/backend/Exceptions/Incorrect_Credidentials.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.Exceptions;
-
-public class Incorrect_Credidentials extends RuntimeException {
-    public Incorrect_Credidentials(){
-        super("Incorrect credidentials");
-    }
-}
